c4d80397ec51abdebeeec46c5709b1a23958065e
DismusM <mwanikidismus1@gmail.com>
2025-07-28 17:36:47 +0300

    deploy: static site to gh-pages

---
+++ _next/static/chunks/pages/_app-4c96c5db68b2646f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{4533:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(6314)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,792],()=>(n(4533),n(1172))),_N_E=_.O()}]);
